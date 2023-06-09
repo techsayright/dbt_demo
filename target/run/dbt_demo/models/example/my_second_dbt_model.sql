d104fc873704e7155b14ea528186fe3f5b9e217d
Darshan Kadiya <60431145+techsayright@users.noreply.github.com>
2023-06-09 12:54:40 +0530

    Add files via upload

---
+++ target/run/dbt_demo/models/example/my_second_dbt_model.sql
@@ -0,0 +1,11 @@
+
+  create or replace   view TEMP.PUBLIC.my_second_dbt_model
+  
+   as (
+    -- Use the `ref` function to select from other models
+
+select *
+from TEMP.PUBLIC.my_first_dbt_model
+where id = 1
+  );
+
